2ab75cc3d26f6ca6a64f24e665c51a42a26b2e8a
Ankit <ankitsharma.as420@gmail.com>
2024-06-12 22:47:05 +0530

    created doctor entity

---
+++ src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Entity/Doctor.java
@@ -0,0 +1,63 @@
+package com.DoctorManagement.DoctorManagementSystemDemo.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String specialization;
+    
+    @Column(name = "contact_info")
+    private String contactInfo;
+
+    public Doctor(){
+
+    }
+
+    public Doctor(String name, String specialization, String contactInfo) {
+        this.name = name;
+        this.specialization = specialization;
+        this.contactInfo = contactInfo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    } 
+}
