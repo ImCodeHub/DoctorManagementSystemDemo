13b32e7881bbbeedad0fdf41a5c09233426a4960
Ankit <ankitsharma.as420@gmail.com>
2024-06-12 23:20:59 +0530

    created doctor model

---
+++ src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Model/DoctorModel.java
@@ -0,0 +1,11 @@
+package com.DoctorManagement.DoctorManagementSystemDemo.Model;
+
+import jakarta.persistence.Column;
+
+public class DoctorModel {
+    private String name;
+
+    private String specialization;
+    
+    private String contactInfo;
+}
