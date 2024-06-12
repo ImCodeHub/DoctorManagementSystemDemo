002854e8d69cc8ac7e6c3eaf2e59d17c585e979b
Ankit <ankitsharma.as420@gmail.com>
2024-06-12 23:58:40 +0530

    added new method to save the doctor details

--- src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Service/DoctorService.java
+++ src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Service/DoctorService.java
@@ -1,5 +1,7 @@
 package com.DoctorManagement.DoctorManagementSystemDemo.Service;
 
+import com.DoctorManagement.DoctorManagementSystemDemo.Model.DoctorModel;
+
 public interface DoctorService {
-    
+    public String addDoctor(DoctorModel doctor);
 }
