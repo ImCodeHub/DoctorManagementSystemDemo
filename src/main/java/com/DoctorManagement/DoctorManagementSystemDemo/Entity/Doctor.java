169daf56400f6fe59dce47440075ad47fcb10f89
Ankit <ankitsharma.as420@gmail.com>
2024-06-12 23:17:22 +0530

    created constructor and getter and setter

--- src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Entity/Doctor.java
+++ src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Entity/Doctor.java
@@ -5,8 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="doctor")
 public class Doctor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -59,5 +61,5 @@ public class Doctor {
 
     public void setContactInfo(String contactInfo) {
         this.contactInfo = contactInfo;
-    } 
+    }  
 }
