74b69e943da7c34c879075d1c62c87ea317b8fd2
Ankit <ankitsharma.as420@gmail.com>
2024-06-12 23:55:49 +0530

    added constructor and getter and setter

--- src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Service/DoctorServiceImpl.java
+++ src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Service/DoctorServiceImpl.java
@@ -1,8 +1,23 @@
 package com.DoctorManagement.DoctorManagementSystemDemo.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.DoctorManagement.DoctorManagementSystemDemo.Entity.Doctor;
+import com.DoctorManagement.DoctorManagementSystemDemo.Exception.GlobalException.DoctorNotFoundException;
+import com.DoctorManagement.DoctorManagementSystemDemo.Model.DoctorModel;
+import com.DoctorManagement.DoctorManagementSystemDemo.Repository.DoctorRepository;
+
 @Service
-public class DoctorServiceImpl {
+public class DoctorServiceImpl implements DoctorService{
+
+    @Autowired
+    DoctorRepository doctorRepository;
+
+    @Override
+    public String addDoctor(DoctorModel doctorModel) {
+        Doctor doctor = new Doctor();
+        doctor.setName(doctorModel.getName());
+    }
     
 }
