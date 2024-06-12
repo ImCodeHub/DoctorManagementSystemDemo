e502e2cb2c741f40b27847cdb87063f0e5f71c07
Ankit <ankitsharma.as420@gmail.com>
2024-06-12 23:58:17 +0530

    added constructor and getter & setter

--- src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Model/DoctorModel.java
+++ src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Model/DoctorModel.java
@@ -6,4 +6,39 @@ public class DoctorModel {
     private String specialization;
     
     private String contactInfo;
+
+    public DoctorModel() {
+    }
+
+    public DoctorModel(String name, String specialization, String contactInfo) {
+        this.name = name;
+        this.specialization = specialization;
+        this.contactInfo = contactInfo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    
 }
