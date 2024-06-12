3506eb00e3082cb2c1c90441c7151d6343b85b29
Ankit <ankitsharma.as420@gmail.com>
2024-06-12 23:38:23 +0530

    created  customer exception handler

---
+++ src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Exception/DoctorExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.DoctorManagement.DoctorManagementSystemDemo.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import com.DoctorManagement.DoctorManagementSystemDemo.Exception.GlobalException.DoctorNotFoundException;
+
+@ControllerAdvice
+public class DoctorExceptionHandler {
+    
+    @ExceptionHandler(DoctorNotFoundException.class)
+    public ResponseEntity<?> handleDoctorNotFoundException(DoctorNotFoundException exception, WebRequest request){
+        return new ResponseEntity<>(exception.getMessage(),HttpStatus.NOT_FOUND);
+    }
+
+}
