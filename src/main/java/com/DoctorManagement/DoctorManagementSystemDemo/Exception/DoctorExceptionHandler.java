75d9597986b25db892b9f8056f2b81e798af0065
Ankit <ankitsharma.as420@gmail.com>
2024-06-13 19:10:08 +0530

    added new method to handle exception for doctor details empty exception

--- src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Exception/DoctorExceptionHandler.java
+++ src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Exception/DoctorExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import com.DoctorManagement.DoctorManagementSystemDemo.Exception.GlobalException.DoctorDetailsEmptyException;
 import com.DoctorManagement.DoctorManagementSystemDemo.Exception.GlobalException.DoctorNotFoundException;
 
 @ControllerAdvice
@@ -16,4 +17,8 @@ public class DoctorExceptionHandler {
         return new ResponseEntity<>(exception.getMessage(),HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(DoctorDetailsEmptyException.class)
+    public ResponseEntity<?> handleDoctorDetailsEmptyException(DoctorDetailsEmptyException exception, WebRequest request){
+        return new ResponseEntity<>(exception.getMessage(),HttpStatus.BAD_REQUEST);
+    }
 }
