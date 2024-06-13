ce4beb7cc175017a47c801b443133d565b2d662d
Ankit <ankitsharma.as420@gmail.com>
2024-06-13 19:08:13 +0530

    created add docotor detail api controller

--- src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Controller/DoctorController.java
+++ src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Controller/DoctorController.java
@@ -2,7 +2,26 @@ package com.DoctorManagement.DoctorManagementSystemDemo.Controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.DoctorManagement.DoctorManagementSystemDemo.Model.DoctorModel;
+import com.DoctorManagement.DoctorManagementSystemDemo.Service.DoctorService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @RestController
+@RequestMapping("api/v1/doctor")
 public class DoctorController {
+    @Autowired
+    DoctorService doctorService;
+
+    @PostMapping("addDoctor")
+    public ResponseEntity<String> addDoctorEntity(@RequestBody DoctorModel doctorModel){
+        String response = doctorService.addDoctor(doctorModel);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
     
 }
