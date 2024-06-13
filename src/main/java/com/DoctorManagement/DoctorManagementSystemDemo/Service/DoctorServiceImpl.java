6d2f029c9c810490ccc1ff0b5a6d9a6d8c519373
Ankit <ankitsharma.as420@gmail.com>
2024-06-13 19:11:53 +0530

    changes in the add doctor method and added validator for empty fields.

--- src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Service/DoctorServiceImpl.java
+++ src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Service/DoctorServiceImpl.java
@@ -1,34 +1,41 @@
 package com.DoctorManagement.DoctorManagementSystemDemo.Service;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.DoctorManagement.DoctorManagementSystemDemo.Entity.Doctor;
-import com.DoctorManagement.DoctorManagementSystemDemo.Exception.GlobalException.DoctorNotFoundException;
+import com.DoctorManagement.DoctorManagementSystemDemo.Exception.GlobalException.DoctorDetailsEmptyException;
+// import com.DoctorManagement.DoctorManagementSystemDemo.Exception.GlobalException.DoctorNotFoundException;
 import com.DoctorManagement.DoctorManagementSystemDemo.Model.DoctorModel;
 import com.DoctorManagement.DoctorManagementSystemDemo.Repository.DoctorRepository;
 
 @Service
-public class DoctorServiceImpl implements DoctorService{
+public class DoctorServiceImpl implements DoctorService {
+
+    private static final Logger logger = LoggerFactory.getLogger(DoctorServiceImpl.class);
 
     @Autowired
     DoctorRepository doctorRepository;
 
+
     @Override
     public String addDoctor(DoctorModel doctorModel) {
-        if(doctorModel!=null){
-            Doctor doctor = new Doctor();
+        Doctor doctor = new Doctor();
+        if (doctorModel != null) {
+            Validator.validDoctorModel(doctorModel);
             doctor.setName(doctorModel.getName());
             doctor.setSpecialization(doctorModel.getSpecialization());
             doctor.setContactInfo(doctorModel.getContactInfo());
-    
+
+            logger.info("doctor data saved {}",doctor);
             doctorRepository.save(doctor);
 
             return "Doctor details has been saved.";
-        }else{
-            return "Doctor details empty please check the details again.";
+        } else {
+            throw new DoctorDetailsEmptyException("doctor details are not found in Object");
         }
-
     }
-    
+
 }
