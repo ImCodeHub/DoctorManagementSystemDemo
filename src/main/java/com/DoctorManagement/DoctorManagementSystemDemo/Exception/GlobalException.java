fad1a4ac8443decc8b38b8c31de68790cabe2e43
Ankit <ankitsharma.as420@gmail.com>
2024-06-13 19:10:48 +0530

    added global exception for empty exception constructor

--- src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Exception/GlobalException.java
+++ src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Exception/GlobalException.java
@@ -7,5 +7,11 @@ public class GlobalException {
         }
     }
 
+    public static class DoctorDetailsEmptyException extends RuntimeException{
+        public DoctorDetailsEmptyException(String message){
+            super(message);
+        }
+    }
+
     // you can create many Global exception class to handle exception as much as you want.
 }
