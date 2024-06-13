c19a374a1d6c320d02355cf4a2575d613cb5868d
Ankit <ankitsharma.as420@gmail.com>
2024-06-13 19:42:39 +0530

    added new method name in service interface to get list of doctors

--- src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Service/DoctorService.java
+++ src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Service/DoctorService.java
@@ -1,7 +1,11 @@
 package com.DoctorManagement.DoctorManagementSystemDemo.Service;
 
+import java.util.List;
+
+import com.DoctorManagement.DoctorManagementSystemDemo.Entity.Doctor;
 import com.DoctorManagement.DoctorManagementSystemDemo.Model.DoctorModel;
 
 public interface DoctorService {
     public String addDoctor(DoctorModel doctor);
+    public List<Doctor> getAllDoctor();
 }
