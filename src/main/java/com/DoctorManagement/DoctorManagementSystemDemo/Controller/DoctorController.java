7d0be1f22b5ddff9b11d16ab9fe4f6581521dc0c
Ankit <ankitsharma.as420@gmail.com>
2024-06-13 19:42:03 +0530

    added new API to get list of doctors

--- src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Controller/DoctorController.java
+++ src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Controller/DoctorController.java
@@ -2,8 +2,11 @@ package com.DoctorManagement.DoctorManagementSystemDemo.Controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.DoctorManagement.DoctorManagementSystemDemo.Entity.Doctor;
 import com.DoctorManagement.DoctorManagementSystemDemo.Model.DoctorModel;
 import com.DoctorManagement.DoctorManagementSystemDemo.Service.DoctorService;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +14,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 @RequestMapping("api/v1/doctor")
@@ -23,5 +28,12 @@ public class DoctorController {
         String response = doctorService.addDoctor(doctorModel);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
+    
+    @GetMapping("allDoctors")
+    public ResponseEntity<List<Doctor>> getDoctors(){
+        List<Doctor> list = doctorService.getAllDoctor();
+        return new ResponseEntity<>(list,HttpStatus.OK);
+    }
+    
     
 }
