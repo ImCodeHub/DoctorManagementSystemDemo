43ba50652c13ae698b0d09186b21c813af690183
Ankit <ankitsharma.as420@gmail.com>
2024-06-14 19:41:17 +0530

    implemented the method to find the specific doctor by the id.

--- src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Service/DoctorServiceImpl.java
+++ src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Service/DoctorServiceImpl.java
@@ -6,8 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import javax.swing.text.html.Option;
-
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,7 +13,6 @@ import org.springframework.stereotype.Service;
 import com.DoctorManagement.DoctorManagementSystemDemo.Entity.Doctor;
 import com.DoctorManagement.DoctorManagementSystemDemo.Exception.GlobalException.DoctorDetailsEmptyException;
 import com.DoctorManagement.DoctorManagementSystemDemo.Exception.GlobalException.DoctorNotFoundException;
-// import com.DoctorManagement.DoctorManagementSystemDemo.Exception.GlobalException.DoctorNotFoundException;
 import com.DoctorManagement.DoctorManagementSystemDemo.Model.DoctorModel;
 import com.DoctorManagement.DoctorManagementSystemDemo.Repository.DoctorRepository;
 
@@ -27,7 +24,6 @@ public class DoctorServiceImpl implements DoctorService {
     @Autowired
     DoctorRepository doctorRepository;
 
-
     @Override
     public String addDoctor(DoctorModel doctorModel) {
         Doctor doctor = new Doctor();
@@ -37,7 +33,7 @@ public class DoctorServiceImpl implements DoctorService {
             doctor.setSpecialization(doctorModel.getSpecialization());
             doctor.setContactInfo(doctorModel.getContactInfo());
 
-            logger.info("doctor data saved {}",doctor);
+            logger.info("doctor data saved {}", doctor);
             doctorRepository.save(doctor);
 
             return "Doctor details has been saved.";
@@ -46,40 +42,37 @@ public class DoctorServiceImpl implements DoctorService {
         }
     }
 
-
     @Override
     public List<Doctor> getAllDoctor() {
         List<Doctor> list = new ArrayList<>();
-        
+
         List<Doctor> doctors = doctorRepository.findAll();
-        if(doctors!=null){
-            for(Doctor doctor : doctors){
+        if (doctors != null) {
+            for (Doctor doctor : doctors) {
                 list.add(doctor);
             }
             return list;
-        }else{
+        } else {
             throw new DoctorNotFoundException("Doctor list not found");
         }
     }
 
-
     @Override
     public Boolean deleteDoctor(Long id) {
         Optional<Doctor> optional = doctorRepository.findById(id);
-        if(optional.isPresent()){
+        if (optional.isPresent()) {
             doctorRepository.deleteById(id);
             return true;
-        }else{
-            logger.error("doctor not found by this id {}",id);
-            throw new DoctorNotFoundException("Doctor not found by this Id - "+id);
+        } else {
+            logger.error("doctor not found by this id {}", id);
+            throw new DoctorNotFoundException("Doctor not found by this Id - " + id);
         }
     }
-
 
     @Override
     public Boolean updateDoctor(Long id, DoctorModel doctorModel) {
         Optional<Doctor> optional = doctorRepository.findById(id);
-        if(optional.isPresent()){
+        if (optional.isPresent()) {
             Doctor doctor = optional.get();
             doctor.setName(doctorModel.getName());
             doctor.setSpecialization(doctorModel.getSpecialization());
@@ -89,7 +82,24 @@ public class DoctorServiceImpl implements DoctorService {
             return true;
         }
         logger.error("Doctor not found by this Id {}", id);
-        throw new DoctorNotFoundException("Doctor not found by this Id - "+id);
+        throw new DoctorNotFoundException("Doctor not found by this Id - " + id);
+    }
+
+    @Override
+    public List<DoctorModel> findDoctorById(Long id) {
+        List<DoctorModel> list = new ArrayList<>();
+        Optional<Doctor> optional = doctorRepository.findById(id);
+        if (optional.isPresent()) {
+            Doctor doctor = optional.get();
+            DoctorModel doctorModel = new DoctorModel();
+            doctorModel.setName(doctor.getName());
+            doctorModel.setSpecialization(doctor.getSpecialization());
+            doctorModel.setContactInfo(doctor.getContactInfo());
+
+            list.add(doctorModel);
+            return list;
+        }
+        throw new DoctorNotFoundException("Doctor not found by this Id - " + id);
     }
 
 }
