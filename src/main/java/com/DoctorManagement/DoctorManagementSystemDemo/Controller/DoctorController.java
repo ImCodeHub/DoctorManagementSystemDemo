f91dc81b2f98401ec9df13b3a909511d8950e804
Ankit <ankitsharma.as420@gmail.com>
2024-06-14 23:32:08 +0530

    removed unneccessory imports

--- src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Controller/DoctorController.java
+++ src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Controller/DoctorController.java
@@ -8,7 +8,6 @@ import com.DoctorManagement.DoctorManagementSystemDemo.Service.DoctorService;
 
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
