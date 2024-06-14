ca2144c4dc174418118f2d854dd93b1abdd8b9e4
Ankit <ankitsharma.as420@gmail.com>
2024-06-14 19:40:10 +0530

    created API to find specific doctor by the id.

--- src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Controller/DoctorController.java
+++ src/main/java/com/DoctorManagement/DoctorManagementSystemDemo/Controller/DoctorController.java
@@ -8,6 +8,7 @@ import com.DoctorManagement.DoctorManagementSystemDemo.Service.DoctorService;
 
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -49,6 +52,11 @@ public class DoctorController {
         Boolean response = doctorService.updateDoctor(id, doctorModel);
         return new ResponseEntity<>(response,HttpStatus.OK);
     }
-    
-    
+
+    @GetMapping("findDoctor/{id}")
+    public ResponseEntity<List<DoctorModel>> findDoctorById(@PathVariable Long id){
+        List<DoctorModel> doctor = doctorService.findDoctorById(id);
+        return new ResponseEntity<>(doctor, HttpStatus.OK);
+    }
+ 
 }
