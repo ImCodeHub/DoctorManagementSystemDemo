fde726c3605c2883837bbd4de1a3387311caf530
Ankit <ankitsharma.as420@gmail.com>
2024-06-17 01:09:12 +0530

    created new properties for the test

---
+++ src/test/java/com/DoctorManagement/DoctorManagementSystemDemo/Repository/DoctorRepositoryTest.java
@@ -0,0 +1,10 @@
+package com.DoctorManagement.DoctorManagementSystemDemo.Repository;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class DoctorRepositoryTest {
+    
+    @Autow
+
+}
